2c62f59a72fec765af78719daae923c7bbd2de20
Ahmad Shapiro <ahmad.shapiro@outlook.com>
2022-10-29 13:36:17 +0200

    Add the google form submission functionality using POST

--- Dates_and_Times/customTests.R
+++ Dates_and_Times/customTests.R
@@ -59,37 +59,38 @@ notify <- function() {
   good <- FALSE
   while(!good) {
     # Get info
-    name <- readline_clean("What is your full name? ")
-    address <- readline_clean("What is the email address of the person you'd like to notify? ")
+    message("\nMake sure you're connected to the internet\n")
+    
+    ID <- readline_clean("What is your ID? ")
     
     # Repeat back to them
     message("\nDoes everything look good?\n")
-    message("Your name: ", name, "\n", "Send to: ", address)
+    message("Your ID: ", ID, "\n")
     
     yn <- select.list(c("Yes", "No"), graphics = FALSE)
     if(yn == "Yes") good <- TRUE
   }
   
   # Get course and lesson names
-  course_name <- attr(e$les, "course_name")
-  lesson_name <- attr(e$les, "lesson_name")
+  course_name <- gsub(" ","+", attr(e$les, "course_name"))
+  lesson_name <- gsub(" ","+", attr(e$les, "lesson_name"))
   
-  subject <- paste(name, "just completed", course_name, "-", lesson_name)
-  body = ""
-  
-  # Send email
-  swirl:::email(address, subject, body)
-  
-  hrule()
-  message("I just tried to create a new email with the following info:\n")
-  message("To: ", address)
-  message("Subject: ", subject)
-  message("Body: <empty>")
-  
-  message("\nIf it didn't work, you can send the same email manually.")
-  hrule()
-  
-  # Return TRUE to satisfy swirl and return to course menu
+  library(httr)
+  baseLink <- "https://docs.google.com/forms/d/e/1FAIpQLScou0ZvvEqj5KHjtuF8dIWqUDC6Pw7wZ1whcI2X8d8ta0vsIw/formResponse?&submit=Submit?usp=pp_url"
+
+  idAttr <- "&entry.2112451087="
+  courseNameAttr <- "&entry.527410779="
+  lessonNameAttr <- "&entry.2005159868="
+
+  toPost <- paste0(baseLink, idAttr, ID, courseNameAttr, course_name, lessonNameAttr, lesson_name)
+
+  response <- POST(toPost) 
+  if (response['status_code'] == 200) {
+    message("\n Successfully Submited")
+  } else {
+    message("\n Please screen shot and contact your instructor")
+  }
+
   TRUE
 }
 
